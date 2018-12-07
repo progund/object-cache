a5dd41868b83a5142f796d360c50a21ff7e287b4
Henrik Sandklef <hesa@sandklef.com>
2018-12-08 00:10:01 +0100

    Arrays more nicely printed

--- se/juneday/ObjectCacheReader.java
+++ se/juneday/ObjectCacheReader.java
@@ -23,38 +23,66 @@ public class ObjectCacheReader<T> {
    *
    */ 
   public void printObject() {
+    if ( cache == null ||
+         cache.readObject() == null ) {
+      System.err.println("cache or cached objects null, nothing to print");
+    } else { 
+      
+      if ( cache.readObject() instanceof Object[] ) {
+        String collectionName ;
+        Object[] objects = (Object[]) cache.readObject();
+        if ( objects == null ) {
+          System.err.println("Array null, nothing to print");
+          return;
+        }
+        if ( objects.length == 0 ) {
+          System.err.println("Array empty, nothing to print");
+          return;
+        }
+        collectionName = objects.getClass().getSimpleName();
+        
+        System.out.println(objects.length +
+                           " cached objects Array[" +
+                           objects[0].getClass().getName()+
+                           "]:");
+        for (int i=0; i<objects.length; i++) {
+          System.out.println(" " + i + ": " + objects[i] );
+        }
+        /*      } else if ( cache.readObject() instanceof Collection ) {
+        String collectionName ;
+        Collection<Object> objects = (Collection<Object>) cache.readObject();
+        if ( objects == null ) {
+          System.err.println("Collection null, nothing to print");
+          return;
+        }
+        if ( objects.size() == 0 ) {
+          System.err.println("Collection empty, nothing to print");
+          return;
+        }
+        collectionName = objects.getClass().getSimpleName();
+        
+        System.out.println( "Cached objects " + collectionName + "<>:" );
+        for (Object o : objects) {
+          System.out.println(" * " + o + "    <" + o.getClass().getName()+ ">");
+          } */
+      }  else {
+        System.out.println();
+        System.out.println("Cached Object: " + cache.readObject().getClass().getName());
+        System.out.println(cache.readObject());
+      }
 
-    if ( cache.readObject() instanceof Collection ) {
-      String collectionName ;
-      Collection<Object> objects = (Collection<Object>) cache.readObject();
-      if ( objects == null ) {
-        System.err.println("Collection null, nothing to print");
-        return;
-      }
-      if ( objects.size() == 0 ) {
-        System.err.println("Collection empty, nothing to print");
-        return;
-      }
-      collectionName = objects.getClass().getSimpleName();
-
-      System.out.println( "Cached objects " + collectionName + "<>:" );
-      for (Object o : objects) {
-        System.out.println(" * " + o + "    <" + o.getClass().getName()+ ">");
-      }
-    } else {
-      System.out.println(" * " + cache.readObject());
     }
   }
-
+  
   /**
    * Retrieves and returns the stored object from
    * file. 
    *
    */ 
   public T object() {
-      return cache.readObject();
+    return cache.readObject();
   }
-
+  
   /**
    * Program to read and print Serialized objects
    *
